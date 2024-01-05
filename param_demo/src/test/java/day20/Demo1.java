3866df636aa206c79233f771e62d61f48d75bcfa
camsu <camsu@host.docker.internal>
2024-01-05 19:01:54 +0530

    updated parameter

--- param_demo/src/test/java/day20/Demo1.java
+++ param_demo/src/test/java/day20/Demo1.java
@@ -1,13 +1,14 @@
 package day20;
 
 import org.testng.Reporter;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Demo1 {
 	@Parameters({"city"})
 	@Test
-	public void testA(String city)
+	public void testA(@Optional("Opune") String city)
 	{ //System.out.println(city);
 		Reporter.log("testA.."+city,true);
 	}
